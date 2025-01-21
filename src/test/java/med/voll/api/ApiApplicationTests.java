54b3f97340ba1b90edbe7e1913edd85300272c8b
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-21 20:09:14 -0300

    fix: Corrigindo teste unitário para não dar erro no build do projeto do docker

--- src/test/java/med/voll/api/ApiApplicationTests.java
+++ src/test/java/med/voll/api/ApiApplicationTests.java
@@ -3,11 +3,6 @@ package med.voll.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
