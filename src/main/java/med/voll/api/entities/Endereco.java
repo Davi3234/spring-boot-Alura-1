da66e369c68b533d5e32ee302f9647a2b4a32205
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-21 21:05:20 -0300

    feat: Adicionando entidade de persistencia de médico e entidade eembeddable e endereço

--- src/main/java/med/voll/api/entities/Endereco.java
+++ src/main/java/med/voll/api/entities/Endereco.java
@@ -1,4 +1,32 @@
 package med.voll.api.entities;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import med.voll.api.dtos.DtoCadastroEndereco;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
 public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco() {
+    }
+
+    public Endereco(DtoCadastroEndereco dtoCadastroEndereco) {
+        this.logradouro = dtoCadastroEndereco.logradouro();
+        this.bairro = dtoCadastroEndereco.bairro();
+        this.cep = dtoCadastroEndereco.cep();
+        this.numero = dtoCadastroEndereco.numero();
+        this.complemento = dtoCadastroEndereco.complemento();
+        this.cidade = dtoCadastroEndereco.cidade();
+        this.uf = dtoCadastroEndereco.uf();
+    }
 }
