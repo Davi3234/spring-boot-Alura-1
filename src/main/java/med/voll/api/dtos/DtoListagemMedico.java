c84e45e6c2c2e4a28732e2079c82aeb85ae0795c
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-27 20:48:46 -0300

    fix: Colocando id no dto de listagem

--- src/main/java/med/voll/api/dtos/DtoListagemMedico.java
+++ src/main/java/med/voll/api/dtos/DtoListagemMedico.java
@@ -3,8 +3,8 @@ package med.voll.api.dtos;
 import med.voll.api.entities.Medico;
 import med.voll.api.enums.Especialidade;
 
-public record DtoListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DtoListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
     public DtoListagemMedico(Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
