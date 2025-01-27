517eae2662a2ad9d4eda6db8e0298b9ef1cfdb65
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-27 20:06:28 -0300

    feat: Criando dataObject de listagem de médico

---
+++ src/main/java/med/voll/api/dtos/DtoListagemMedico.java
@@ -0,0 +1,10 @@
+package med.voll.api.dtos;
+
+import med.voll.api.entities.Medico;
+import med.voll.api.enums.Especialidade;
+
+public record DtoListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+    public DtoListagemMedico(Medico medico){
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
