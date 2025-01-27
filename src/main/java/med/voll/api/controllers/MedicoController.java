be41b85daed96e9d231d6692683e9d32d705b1d3
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-27 20:06:59 -0300

    feat: Mudando tipo de retorno e adicionando paginação no get de médicos

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -2,9 +2,12 @@ package med.voll.api.controllers;
 
 import jakarta.validation.Valid;
 import med.voll.api.dtos.DtoCadastroMedico;
+import med.voll.api.dtos.DtoListagemMedico;
 import med.voll.api.entities.Medico;
 import med.voll.api.repositories.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Medico>> list() {
-        System.out.println(medicoRepository.findAll());
-        return ResponseEntity.ok(medicoRepository.findAll());
+    public Page<DtoListagemMedico> list(Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DtoListagemMedico::new);
     }
 }
